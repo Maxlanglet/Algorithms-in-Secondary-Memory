3847d2fb05bc5c0c00e2d7c089eb390fcc41b9bb
Ghita-Ait-Ouhmane <gaitouhm@ulb.ac.be>
2020-11-28 12:44:47 +0100

    read2 length

--- InputStream2.cpp
+++ InputStream2.cpp
@@ -1,13 +1,12 @@
 #include "InputStream2.hpp"
 
-InputStream2::InputStream2(const char * filename){
-	path = filename;
+InputStream2::InputStream2(){
 }
 
 InputStream2::~InputStream2(){}
 
 
-void InputStream2::open(){
+void InputStream2::open(const char* path){
 	// open the file
 	file = fopen (path , "r");
 	if (file == NULL) perror ("Error opening file");
@@ -16,30 +15,35 @@ void InputStream2::open(){
 
 
 //reads in a buffer
-void InputStream2::readln(){
+int InputStream2::readln(){ //returns length of file read
 	// initialize a buffer of fixed size 100
 	char buffer [100];
 	// if we are not at the end of the file, we read the size of the buffer
 	 if ( ! feof (file) ){
        
 	   //while(buffer[99] != '\n' && buffer[99]!='\0')
-		if ( fgets (buffer , 100 , file) == NULL ) cout<<"error, NULL"<<endl;
-		// verify that we indeed read the right characters
-       	fputs (buffer , stdout);
-		
+		if ( fgets (buffer , 100 , file) == NULL ){
+			//cout<<"error, NULL"<<endl;
+			return 0;
+		} return strlen(buffer);
      }
-	 /*
-	 // this is the code to read the entire file
-     while ( ! feof (file) ){
-       if ( fgets (buffer , 100 , file) == NULL ) break;
-       fputs (buffer , stdout);
-     }*/
 }
 
 
 
 void InputStream2::seek(int pos){
 	fseek(file,pos,SEEK_SET);
+}
+
+int InputStream2::length(string filename){
+	open(filename.c_str());
+	int sum = 0 ;
+	int line_size = 1 ;
+	 while (line_size > 0 ){
+		line_size = readln();
+		sum += line_size ;
+	 }
+	 return sum ;
 }
 
 bool InputStream2::end_of_stream(){
