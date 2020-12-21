34574fdc4c6ec4adc757edff9dab83740c526259
Ghita-Ait-Ouhmane <gaitouhm@ulb.ac.be>
2020-12-21 11:02:10 +0100

    changed opening mode in outstreamm4 from append to trunc

--- OutputStream4.cpp
+++ OutputStream4.cpp
@@ -15,7 +15,7 @@ void OutputStream4::create(string filename){
 	path = filename;
 	mode_t mode = S_IRUSR | S_IWUSR | S_IRGRP | S_IROTH;
 	//new_file = open(path.c_str(), O_RDWR | O_APPEND| O_CREAT , mode);
-	new_file = open(path.c_str(), O_RDWR | O_APPEND| O_CREAT , mode);
+	new_file = open(path.c_str(), O_RDWR | O_TRUNC| O_CREAT , mode);
 	if (new_file == -1){
 	   handle_error("open");
 	}
@@ -35,7 +35,7 @@ void OutputStream4::writeln(string str){
 
 	int filesize = lseek(new_file, 0, SEEK_END);
 	
-	printf("filesize is %d\n",filesize);
+
 
 	// size of our map
 	size_t B = getpagesize();
@@ -45,16 +45,12 @@ void OutputStream4::writeln(string str){
 	
 	int pagesize = getpagesize();
 	int numberpages = (-1+pagesize+textsize)/pagesize;
-	printf("textsize is %ld, pagsize is %d, number of pages is %d\n",textsize,pagesize, numberpages);
-	printf("B is %ld, pagsize is %d, loop is %ld\n",B,pagesize, numberpages*pagesize/B);
 	int result = ftruncate(new_file, (textsize-1)+filesize);
 	//lseek(new_file, filesize, SEEK_SET);
 	int leftover = textsize-(numberpages * pagesize);
 	int numberpagesfile = (-1+pagesize+filesize)/pagesize;
 	if(numberpagesfile!=0 && (filesize%pagesize)!=0){numberpagesfile--;}
-	cout << typeid(numberpagesfile).name() << endl;
 	int restonpage = filesize-(numberpagesfile*pagesize);
-	printf("pagefile is %d, pagsize is %d, loop is %ld, nb rest %d\n",numberpagesfile,pagesize, numberpages*pagesize/B,restonpage);
 	
 	
 	//in case B is too big
@@ -67,8 +63,7 @@ void OutputStream4::writeln(string str){
 	// this will loop until we reach the end of the string, if the offset is equal or greater than
 	// the size of the string, we finished to write everything
 	while(loop < numberpages*pagesize/B){
-		printf("offset is %zu \n", offset);
-
+		
 		// now we can map the file, we map a size B and begin at the offset
 		char* map = static_cast<char*>(mmap(NULL, B, PROT_READ | PROT_WRITE,MAP_SHARED, new_file, ((numberpagesfile+loop)*getpagesize())));
 		if (map == MAP_FAILED){
@@ -77,7 +72,6 @@ void OutputStream4::writeln(string str){
 			exit(EXIT_FAILURE);
 		}
 
-		printf("0 \n");
 		// add the char in the map
 		// if there is still enough space to read an entire map of size B
 		if(loop==0){
@@ -85,15 +79,12 @@ void OutputStream4::writeln(string str){
 			// write B minus what is already on the page char of the string in the map
 			// offset here will always be 0
 			for (size_t j = 0; j < (B-restonpage); j++){
-				printf("CASE1-1 Writing character %c at %zu\n", str[j], j);
 				map[restonpage+j] = str[j];
 			}
-			printf("after writing \n");
 			}
 			else{
 				// write the char remaining in the map (not of size B)
 				for (size_t j = 0; j < textsize-1; j++){
-					printf("CASE2-1 Writing character %c at %zu\n", str[j], j);
 					//map[restonpage+((textsize-1))+j] = str[j];
 					map[restonpage+j] = str[j];
 				}
@@ -103,18 +94,15 @@ void OutputStream4::writeln(string str){
 			if((textsize-offset+restonpage)>=B){
 			// write B char of the string in the map
 			for (size_t j = 0; j < B; j++){
-				printf("CASE1-2 Writing character %c at %zu\n", str[j+offset], j);
 				map[j] = str[j+offset];
 			}
 
 			// write the entire string (size of B)
 			//memcpy(map, str.c_str(), B);
-			printf("after writing \n");
 			}
 			else{
 				// write the char remaining in the map (not of size B)
 				for (size_t j = 0; j < (textsize-offset+restonpage-1); j++){
-					printf("CASE2-2 Writing character %c at %zu\n", str[j+offset], j);
 					//map[((textsize-offset-1))+j] = str[j+offset];
 					map[j] = str[j+offset-restonpage];
 				}
@@ -124,9 +112,7 @@ void OutputStream4::writeln(string str){
 			//add offset to only copy where we are in the string
 			// textsize-offset-1 is the rest
 			//memcpy(map, str.c_str()+offset, textsize-offset-1);
-			printf("after writing2 \n");
 		}
-		printf("1 \n");
 		/*for (size_t j = 0; j < textsize-offset-1; j++){
 				printf("Writing character %c\n", map[j]);
 		}*/
@@ -135,14 +121,12 @@ void OutputStream4::writeln(string str){
 		if (msync(map, B, MS_SYNC) == -1){
 			perror("Could not sync the file to disk");
 		}
-		printf("2 \n");
 		// Don't forget to free the mmapped memory
 		if (munmap(map, textsize) == -1){
 			close(new_file);
 			perror("Error un-mmapping the file");
 			exit(EXIT_FAILURE);
 		}
-		printf("3 \n");
 		// incremente the offset of B
 		offset += B;
 		loop+=1;
--- main.cpp
+++ main.cpp
@@ -40,13 +40,14 @@ cout << "1 "<<" & ";
 cout << size << " & ";
 cout << length << " & ";
 cout <<  average/4 << " & "<< length/(average/4) << " \\\\" << endl;
-
+*/
+/*
 {
 	Timer timer;
 	cout << "sum rand is " << instream.randjump(title,1000) << endl;
 }
-
-	instream.close();
+*/
+/*instream.close();
 
 
 	//printf("TEST INPUTSTREAM2\n\n");
@@ -66,12 +67,13 @@ for(int i=0; i<4; i++){
 cout << "2 "<<" & ";
 cout << " & "<< length2 << " & ";
 cout <<  average2/4 << " & "<< length2/(average2/4) << " \\\\" << endl;
-
+*/
+/*
 {
 	Timer timer;
 	cout << "sum rand is " << instream2.randjump(title,1000) << endl;
-}
-	instream2.close();
+}*/
+/*	instream2.close();
 
 
 	//printf("TEST INPUTSTREAM3\n\n");
@@ -90,15 +92,16 @@ for(int i=0; i<4; i++){
 cout << "3 "<<" & ";
 cout << " & "<< length3 << " & ";
 cout <<  average3/4 << " & "<< length3/(average3/4) << " \\\\" << endl;
-
+*/
+/*
 {
 	Timer timer;
 	cout << "sum rand is " << instream3.randjump(title,1000,size) << endl;
 }
 
 	instream3.close();
-
-	instream3.close();
+*/
+/*	instream3.close();
 	int mul=1;
 
 	if (size >= 4096){
@@ -124,24 +127,30 @@ for(int i=0; i<4; i++){
 cout << "4 "<<" & ";
 cout << " & "<< length4 << " & ";
 cout <<  average4/4 << " & "<< length4/(average4/4) << " \\\\" << endl;
-
+*/
+/*
 {
 	Timer timer;
 	cout << "sum rand is " << instream4.randjump(title,1000) << endl;
-}
+}*/
 
-	instream4.close();
+/*	instream4.close();
 
 	cout << "\\hline" << endl;
 */
 
-/*
+
 	printf("TEST RRMERGE4\n\n");
 	RrmergeMethod4 r;
-	r.rrmerge1("theoutput",2,"role_type.csv","test.csv");
 
+	
+	r.rrmerge1("theoutput1.txt",3,"info_type.csv","role_type.csv","test.csv");
+	r.rrmerge2("theoutput2.txt",3,"info_type.csv","role_type.csv","test.csv");
+	r.rrmerge3("theoutput3.txt",3,"info_type.csv","role_type.csv","test.csv");
+	r.rrmerge4("theoutput4.txt",3,"info_type.csv","role_type.csv","test.csv");
+	
 
-
+/*
 
 	printf("TEST INPUTSTREAM3\n\n");
 	InputStream3 instream3 ; //if () considers it a function in main 
@@ -183,15 +192,15 @@ cout <<  average4/4 << " & "<< length4/(average4/4) << " \\\\" << endl;
 	OutputStream3 outstream3;
 	outstream3.create(title2.c_str());
 	outstream3.writeln("heololo");*/
-	
+	/*
 	string title4="shorttext.txt";
-	OutputStream4 outstream4= OutputStream4();
-	outstream4.create(title4.c_str());
+	OutputStream4 outstream4(title4.c_str());
+	outstream4.create();
 	//outstream4.open2();
-	//string str = "Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim. Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu. In enim justo, rhoncus ut, imperdiet a, venenatis vitae, justo. Nullam dictum felis eu pede mollis pretium. Integer tincidunt. Cras dapibus. Vivamus elementum semper nisi. Aenean vulputate eleifend tellus. Aenean leo ligula, porttitor eu, consequat vitae, eleifend ac, enim. Aliquam lorem ante, dapibus in, viverra quis, feugiat a, tellus. Phasellus viverra nulla ut metus varius laoreet. Quisque rutrum. Aenean imperdiet. Etiam ultricies nisi vel augue. Curabitur ullamcorper ultricies nisi. Nam eget dui. Etiam rhoncus. Maecenas tempus, tellus eget condimentum rhoncus, sem quam semper libero, sit amet adipiscing sem neque sed ipsum. Nam quam nunc, blandit vel, luctus pulvinar, hendrerit id, lorem. Maecenas nec odio et ante tincidunt tempus. Donec vitae sapien ut libero venenatis faucibus. Nullam quis ante. Etiam sit amet orci eget eros faucibus tincidunt. Duis leo. Sed fringilla mauris sit amet nibh. Donec sodales sagittis magna. Sed consequat, leo eget bibendum sodales, augue velit cursus nunc, quis gravida magna mi a libero. Fusce vulputate eleifend sapien. Vestibulum purus quam, scelerisque ut, mollis sed, nonummy id, metus. Nullam accumsan lorem in dui. Cras ultricies mi eu turpis hendrerit fringilla. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; In ac dui quis mi consectetuer lacinia. Nam pretium turpis et arcu. Duis arcu tortor, suscipit eget, imperdiet nec, imperdiet iaculis, ipsum. Sed aliquam ultrices mauris. Integer ante arcu, accumsan a, consectetuer eget, posuere ut, mauris. Praesent adipiscing. Phasellus ullamcorper ipsum rutrum nunc. Nunc nonummy metus. Vestibulum volutpat pretium libero. Cras id dui. Aenean ut eros et nisl sagittis vestibulum. Nullam nulla eros, ultricies sit amet, nonummy id, imperdiet feugiat, pede. Sed lectus. Donec mollis hendrerit risus. Phasellus nec sem in justo pellentesque facilisis. Etiam imperdiet imperdiet orci. Nunc nec neque. Phasellus leo dolor, tempus non, auctor et, hendrerit quis, nisi. Curabitur ligula sapien, tincidunt non, euismod vitae, posuere imperdiet, leo. Maecenas malesuada. Praesent congue erat at massa. Sed cursus turpis vitae tortor. Donec posuere vulputate arcu. Phasellus accumsan cursus velit. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Sed aliquam, nisi quis porttitor congue, elit erat euismod orci, ac placerat dolor lectus quis orci. Phasellus consectetuer vestibulum elit. Aenean tellus metus, bibendum sed, posuere ac, mattis non, nunc. Vestibulum fringilla pede sit amet augue. In turpis. Pellentesque posuere. Praesent turpis. Aenean posuere, tortor sed cursus feugiat, nunc augue blandit nunc, eu sollicitudin urna dolor sagittis lacus. Donec elit libero, sodales nec, volutpat a, suscipit non, turpis. Nullam sagittis. Suspendisse pulvinar, augue ac venenatis condimentum, sem libero volutpat nibh, nec pellentesque velit pede quis nunc. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Fusce id purus. Ut varius tincidunt libero. Phasellus dolor. Maecenas vestibulum mollis diam. Pellentesque ut neque. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. In dui magna, posuere eget, vestibulum et, tempor auctor, justo. In ac felis quis tortor malesuada pretium. Pellentesque auctor neque nec urna. Proin sapien ipsum, porta a, auctor quis, euismod ut, mi. Aenean viverra rhoncus pede. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Ut non enim eleifend felis pretium feugiat. Vivamus quis mi. Phasellus a est. Phasellus magna. In hac habitasse platea dictumst. Curabitur at lacus ac velit ornare lobortis. Cura yoyooyoyoyo";
-	string str = "hello";
+	string str = "Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim. Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu. In enim justo, rhoncus ut, imperdiet a, venenatis vitae, justo. Nullam dictum felis eu pede mollis pretium. Integer tincidunt. Cras dapibus. Vivamus elementum semper nisi. Aenean vulputate eleifend tellus. Aenean leo ligula, porttitor eu, consequat vitae, eleifend ac, enim. Aliquam lorem ante, dapibus in, viverra quis, feugiat a, tellus. Phasellus viverra nulla ut metus varius laoreet. Quisque rutrum. Aenean imperdiet. Etiam ultricies nisi vel augue. Curabitur ullamcorper ultricies nisi. Nam eget dui. Etiam rhoncus. Maecenas tempus, tellus eget condimentum rhoncus, sem quam semper libero, sit amet adipiscing sem neque sed ipsum. Nam quam nunc, blandit vel, luctus pulvinar, hendrerit id, lorem. Maecenas nec odio et ante tincidunt tempus. Donec vitae sapien ut libero venenatis faucibus. Nullam quis ante. Etiam sit amet orci eget eros faucibus tincidunt. Duis leo. Sed fringilla mauris sit amet nibh. Donec sodales sagittis magna. Sed consequat, leo eget bibendum sodales, augue velit cursus nunc, quis gravida magna mi a libero. Fusce vulputate eleifend sapien. Vestibulum purus quam, scelerisque ut, mollis sed, nonummy id, metus. Nullam accumsan lorem in dui. Cras ultricies mi eu turpis hendrerit fringilla. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; In ac dui quis mi consectetuer lacinia. Nam pretium turpis et arcu. Duis arcu tortor, suscipit eget, imperdiet nec, imperdiet iaculis, ipsum. Sed aliquam ultrices mauris. Integer ante arcu, accumsan a, consectetuer eget, posuere ut, mauris. Praesent adipiscing. Phasellus ullamcorper ipsum rutrum nunc. Nunc nonummy metus. Vestibulum volutpat pretium libero. Cras id dui. Aenean ut eros et nisl sagittis vestibulum. Nullam nulla eros, ultricies sit amet, nonummy id, imperdiet feugiat, pede. Sed lectus. Donec mollis hendrerit risus. Phasellus nec sem in justo pellentesque facilisis. Etiam imperdiet imperdiet orci. Nunc nec neque. Phasellus leo dolor, tempus non, auctor et, hendrerit quis, nisi. Curabitur ligula sapien, tincidunt non, euismod vitae, posuere imperdiet, leo. Maecenas malesuada. Praesent congue erat at massa. Sed cursus turpis vitae tortor. Donec posuere vulputate arcu. Phasellus accumsan cursus velit. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Sed aliquam, nisi quis porttitor congue, elit erat euismod orci, ac placerat dolor lectus quis orci. Phasellus consectetuer vestibulum elit. Aenean tellus metus, bibendum sed, posuere ac, mattis non, nunc. Vestibulum fringilla pede sit amet augue. In turpis. Pellentesque posuere. Praesent turpis. Aenean posuere, tortor sed cursus feugiat, nunc augue blandit nunc, eu sollicitudin urna dolor sagittis lacus. Donec elit libero, sodales nec, volutpat a, suscipit non, turpis. Nullam sagittis. Suspendisse pulvinar, augue ac venenatis condimentum, sem libero volutpat nibh, nec pellentesque velit pede quis nunc. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Fusce id purus. Ut varius tincidunt libero. Phasellus dolor. Maecenas vestibulum mollis diam. Pellentesque ut neque. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. In dui magna, posuere eget, vestibulum et, tempor auctor, justo. In ac felis quis tortor malesuada pretium. Pellentesque auctor neque nec urna. Proin sapien ipsum, porta a, auctor quis, euismod ut, mi. Aenean viverra rhoncus pede. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Ut non enim eleifend felis pretium feugiat. Vivamus quis mi. Phasellus a est. Phasellus magna. In hac habitasse platea dictumst. Curabitur at lacus ac velit ornare lobortis. Cura yoyooyoyoyo";
+	//string str = "hello";
 	outstream4.writeln(str.c_str());
 	outstream4.close2();
-	
+	*/
 	return 0;
 }
